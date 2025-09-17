7f6af0bac35a1b23139a4d9c5453a11ebcb6fbd1
Kyle <Archaon@byui.edu>
2025-09-17 08:28:01 -0600

    sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("What's Your Name?");
+        string user_name = ("Kyle");
+        Console.WriteLine("hello, ",user_name);
     }
 }
